fdb0264808efa0287d9db980fd5fe77a89d72921
sibofeng <fengsiboacer@gmail.com>
2018-03-29 17:37:46 +0800

    add qq&163

--- 163_car.py
+++ 163_car.py
@@ -2,14 +2,15 @@ import requests
 import re, os, time
 from selenium import webdriver
 from lxml import etree
+from selenium.webdriver.support.select import Select
 
 
 class spider():
     def __init__(self, main_page, save_path):
         self.main_page = main_page
         self.save_path = save_path
-        self.driver = webdriver.PhantomJS('./phantomjs.exe')
-        # self.driver = webdriver.Chrome('./chromedriver.exe')
+        # self.driver = webdriver.PhantomJS('./phantomjs.exe')
+        self.driver = webdriver.Chrome('./chromedriver.exe')
 
     def analysis_main_page(self):
         select, path = self.get_selector_and_path(self.main_page, self.save_path)
@@ -19,20 +20,88 @@ class spider():
         print(len(hrefs))
         for i, (name, href) in enumerate(zip(names, hrefs)):
             print(i, name, href)
+            self.sub_page(href, path, name)
 
-    def sub_page(self, url, path, name):
-        pass
+    def sub_page(self, url, path, name=None):
+        selector, save_path = self.get_selector_and_path(self.main_page + url, path, name)
+        containers = selector.xpath('//div[@class="c-bd"]/div[1]')
+        for container in containers:
+            blocks = container.xpath('./div')
+            sub_names = container.xpath('./div/p/text()')
+            for block, sub_name in zip(blocks, sub_names):
+                items = block.xpath('./ul/li')
+                for item in items:
+                    subsub_names = item.xpath('./p[@class="title"]/a/text()')
+                    hrefs = item.xpath('./p[@class="photo"]/a/@href')
+                    new_path = os.path.join(save_path, sub_name)
+                    for subsub_name, href in zip(subsub_names, hrefs):
+                        # print(subsub_name, href, new_path)
+                        self.click_all_image(href, new_path, subsub_name)
 
+    # 点击 全部图片
+    def click_all_image(self, url, path, name=None):
+        selector, save_path = self.get_selector_and_path(self.main_page + url, path, name)
+        hrefs = selector.xpath('//div[@class="status"]/a/@href')
+        if not len(hrefs) == 0:
+            self.select_years(hrefs[0], save_path)
 
+    def select_years(self, url, path, name=None):
+        selector, save_path = self.get_selector_and_path(self.main_page + url, path, name)
+        option_values = selector.xpath('//select[@id="yeartype_select"]/option/@value')
+        if not len(option_values) == 0:
+            for option_value in option_values:
+                self.driver.get(self.main_page + url)
+                option_years = Select(self.driver.find_element_by_id('yeartype_select'))
+                option_years.select_by_value(option_value)  # 容易导致元素链接失败
+                time.sleep(0.1)
+                year_url = self.driver.current_url
+                self.select_type(year_url, save_path, option_value)
 
-    def get_selector_and_path(self, url, save_path, name=None):
+    # 选择汽车类型
+    def select_type(self, url, path, name=None):
+        selector, save_path = self.get_selector_and_path(url, path, name)
+        option_values = selector.xpath('//select[@id="autoproduct_select"]/option/@value')
+        option_names = selector.xpath('//select[@id="autoproduct_select"]/option/text()')
+        if not len(option_values) == 0:
+            for option_value, option_name in zip(option_values, option_names[1:]):
+                self.driver.get(url)
+                option_types = Select(self.driver.find_element_by_id('autoproduct_select'))
+                option_types.select_by_value(option_value)
+                time.sleep(0.25)
+                print("---->", self.driver.current_url, option_name)
+                self.subsub_page(self.driver.current_url, save_path, option_name)
+
+    def subsub_page(self, url, path, name=None):
+        selector, save_path = self.get_selector_and_path(url, path, name)
+        if selector is not None:
+            directions = selector.xpath('//div[@class="bd"]/ul[@class="clearfix"]/li/a/h4/text()')
+            hrefs = selector.xpath('//div[@class="bd"]/ul[@class="clearfix"]/li/a/@href')
+            if not len(hrefs) == 0:
+                for direction, href in zip(directions, hrefs):
+                    if direction in ['左前', '正前', '正侧', '左后', '正后', '右后']:
+                        self.large_scale_pic(href, save_path, direction)
+
+    def large_scale_pic(self, url, path, name=None):
+        selector, save_path = self.get_selector_and_path(url, path, name)
+        # hrefs = selector.xpath('//img[@class="low_photo"]/@src')
+        hrefs = selector.xpath('//img[@class="main_photo hidden"]/@src')
+        if not len(hrefs) == 0:
+            self.save_pic(hrefs[0], save_path)
+
+    def get_selector_and_path(self, url, save_path, name=None, sleep=0):
         sub_url = url
         if not name is None:
             path = os.path.join(save_path, name)
         else:
             path = save_path
         self.make_dir(path)
-        self.driver.get(sub_url)
+        try:
+            self.driver.set_page_load_timeout(10000)
+            self.driver.get(sub_url)
+        except:
+            print("\033[31;0m[URL ERROR] TIME_OUT\033[0m")
+            return None, path
+        time.sleep(sleep)
         sub_page = self.driver.page_source
         selector = etree.HTML(sub_page)
         return selector, path
@@ -63,6 +132,10 @@ class spider():
 
 
 if __name__ == "__main__":
-    sp = spider('http://product.auto.163.com/', './163_download')
-    # sp.analysis_main_page()
-    sp.sub_page()
+    sp = spider('http://product.auto.163.com', './163_download')
+    sp.analysis_main_page()
+    # sp.sub_page('/new_daquan/brand/1685.html', './163_download/奥迪')
+    # sp.click_all_image('/series/3148.html#new18list', './163_download/奥迪/一汽-大众奥迪', '奥迪Q5')
+    # sp.subsub_page('/series/photo/3148.html#ncx00040', './163_download/奥迪/一汽-大众奥迪/奥迪Q5')
+    # sp.large_scale_pic('http://product.auto.163.com/picture/photoview/2AF90008/193635/CQBHDQVT2AF90008.html',
+    #                    './163_download/奥迪/一汽-大众奥迪/奥迪Q5')
--- tencent_car.py
+++ tencent_car.py
@@ -2,31 +2,103 @@ import requests
 import re, os, time
 from selenium import webdriver
 from lxml import etree
+import platform
 
 
+# 有问题 腾讯汽车车型怕不下来 页面总是加载超时 不知道什么问题
 class spider():
     def __init__(self, main_page, save_path):
         self.main_page = main_page
         self.save_path = save_path
-        self.driver = webdriver.PhantomJS('./phantomjs.exe')
-        # self.driver = webdriver.Chrome('./chromedriver.exe')
+        sys_type = platform.system()
+        if sys_type == "Windows":
+            # self.driver = webdriver.PhantomJS('./phantomjs.exe')
+            self.driver = webdriver.Chrome('./chromedriver.exe')
+        elif sys_type == "Linux":
+            self.driver = webdriver.PhantomJS('/home1/fsb/env2/phantomjs-2.1.1-linux-x86_64/bin/phantomjs')
+            # self.driver = webdriver.Firefox('/home1/fsb/env2/firefox/geckodriver')
 
     def analysis_main_page(self):
         select, path = self.get_selector_and_path(self.main_page, self.save_path, sleep=5)
         # containers = select.xpath('//brandlogo/div/a[2]/text()')
-        containers = select.xpath('//div[@class="listAll"]')
-        print(len(containers))
-        for container in containers:
-            print(container)
+        if select is not None:
+            containers = select.xpath('//div[@class="listAll"]')
+            # main_names = select.xpath('//div[@class="listLogo"]/a[2]/text()')
+            for i, container in enumerate(containers):
+                # if i < 3:
+                main_names = container.xpath('./brandlogo/div[@class="listLogo"]/a[2]/text()')
+                print(main_names)
+                for main_name in main_names:
+                    names = container.xpath('./div[@class="listData"]/manname/h3/a/text()')
+                    print(names)
+                    listDatas = container.xpath('./div[@class="listData"]')
+                    for name, listData in zip(names, listDatas):
+                        sub_names = listData.xpath('./ul/li/a/text()')
+                        print(sub_names)
+                        hrefs = listData.xpath('./ul/li/a/@href')
+                        for sub_name, href in zip(sub_names, hrefs):
+                            path = os.path.join(self.save_path, main_name, name)
+                            print(i, href, path, sub_name)
+                            self.click_image(href, path, sub_name)
+                            # time.sleep(0.1)
 
-    def get_selector_and_path(self, url, save_path, name=None, sleep=0):
+    def click_image(self, url, path, name):
+        select, save_path = self.get_selector_and_path(url, path, name, sleep=0)
+        if select is not None:
+            hrefs = select.xpath('//li[@id="serial_pic"]/a/@href')
+            if not len(hrefs) == 0:
+                self.subsubsub_page(hrefs[0], save_path)
+
+    def subsubsub_page(self, url, path, name=None):
+        select, save_path = self.get_selector_and_path(url, path, name)
+        if select is not None:
+            hrefs = select.xpath('//div[@id="photo_list_wg"]/ul/li/a/@href')
+            sub_names = select.xpath('//div[@id="photo_list_wg"]/ul/li/h4/text()')
+            if hrefs is not None and sub_names is not None:
+                for href, sub_name in zip(hrefs, sub_names):
+                    print("--->", href, sub_name)
+                    self.large_scale_image(href, save_path, sub_name)
+
+    def large_scale_image(self, url, path, name):
+        sub_url = url
+        if not name is None:
+            save_path = os.path.join(path, name)
+        else:
+            save_path = path
+        self.make_dir(path)
+        try:
+            self.driver.set_page_load_timeout(10000)
+            sys_type = platform.system()
+            if sys_type == "Windows":
+                # self.driver = webdriver.PhantomJS('./phantomjs.exe')
+                self.driver = webdriver.Chrome('./chromedriver.exe')
+            elif sys_type == "Linux":
+                self.driver = webdriver.PhantomJS('/home1/fsb/env2/phantomjs-2.1.1-linux-x86_64/bin/phantomjs')
+            self.driver.get(sub_url)
+        except:
+            print("\033[31;0m[URL ERROR] TIME_OUT\033[0m")
+            return
+        time.sleep(1)
+        sub_page = self.driver.page_source
+        select = etree.HTML(sub_page)
+        if select is not None:
+            hrefs = select.xpath('//img[@id="PicSrc"]/@src')
+            if not len(hrefs) == 0:
+                self.save_pic(hrefs[0], save_path)
+
+    def get_selector_and_path(self, url, save_path, name=None, sleep=1):
         sub_url = url
         if not name is None:
             path = os.path.join(save_path, name)
         else:
             path = save_path
         self.make_dir(path)
-        self.driver.get(sub_url)
+        try:
+            self.driver.set_page_load_timeout(10000)
+            self.driver.get(sub_url)
+        except:
+            print("\033[31;0m[URL ERROR] TIME_OUT\033[0m")
+            return None, path
         time.sleep(sleep)
         sub_page = self.driver.page_source
         selector = etree.HTML(sub_page)
@@ -44,13 +116,8 @@ class spider():
             if not os.path.exists(save_path):
                 os.makedirs(save_path)
 
-            try:
-                re_get = requests.get(url, timeout=timeout)
-            except requests.exceptions.ConnectTimeout:
-                print("connect time out")
-            except requests.exceptions.Timeout:
-                print("download time out")
-
+            re_get = requests.get(url, timeout=timeout)
+            time.sleep(1)
             with open(file_path, "wb") as file:
                 file.write(re_get.content)
         except Exception:
@@ -58,5 +125,8 @@ class spider():
 
 
 if __name__ == "__main__":
-    sp = spider('http://data.auto.qq.com/car_brand/index.shtml', './qq_download')
+    sp = spider('http://data.auto.qq.com/car_brand/index.shtml', './tencent_download')
     sp.analysis_main_page()
+    # sp.subsubsub_page('http://data.auto.qq.com/car_serial/1545' + '/serialpic_nl.shtml', './tencent_download\AC Schnitzer', ' AC Schnitzer M3')
+    # sp.large_scale_iamge('http://data.auto.qq.com/car_public/1/disp_pic_nl.shtml#sid=1545&tid=1&pid=1930524',
+    #                      './qq_download\AC Schnitzer\AC Schnitzer M3', '亚琛施纳泽 M3 资料图')
